c310faa0f309e3e78d01c51c3905cd0c02dc87e6
Елдашев Кирилл Юрьевич <212208@mck-ktits.ru>
2024-09-03 16:45:28 +0300

    Добавьте файлы проекта.

---
+++ control/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace control
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public List<Person> list = new List<Person>();
+
+       
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Regex regex = new Regex(@"^[А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+$");
+            Match match = regex.Match(firstName.Text);
+            DateTime temp;
+            double com = 0;
+            double c = 0;
+            DateTime oneYearBefore = DateTime.Now.AddYears(-3);
+            DateTime dateNow = DateTime.Now;
+
+            var parsedDate = DateTime.Parse(data.Text);
+
+            if (match.Success)
+            {            
+                if (DateTime.TryParse(data.Text, out temp))
+                {
+                    if(Convert.ToInt32(revenue.Text) < 50000 )
+                    {
+                        com = Convert.ToInt32(revenue.Text) * 0.03;
+                    }
+                    else if(Convert.ToInt32(revenue.Text) >= 50000)
+                    {
+                        com = Convert.ToInt32(revenue.Text) * 0.06;
+                    }
+
+                    if(oneYearBefore > parsedDate)
+                    {
+                        c += com + com * 0.05;
+                        commissions.Text = Convert.ToString(c);
+                    }
+                }
+                else
+                {
+                    data.Text = "Неверный формат";
+                }
+            }
+            else
+            {
+                firstName.Text = "Неверный формат";
+            }
+
+            Person person = new Person(firstName.Text, c, Convert.ToInt32(revenue.Text), Convert.ToString(dateNow - parsedDate));
+            list.Add(person);
+
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+    }
+}
---
+++ control/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace control
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public double Commissions { get; set; }
+        public int Revenue { get; set; }
+        public string Seniority { get; set; }
+
+        public Person(string name, double comi, int rev, string sen)
+        {
+            Name = name;
+            Commissions = comi;
+            Revenue = rev;
+            Seniority = sen;
+        }
+    }
+}
